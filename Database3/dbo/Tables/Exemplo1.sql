06c759c6155c95928be3d2b47edf77f95c60b5e7
Arthur <Arthur@ARTHUR>
2017-11-06 23:08:25 -0200

    Adicionado o projeto do banco de dados (2)

---
+++ Database3/dbo/Tables/Exemplo1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Exemplo1] (
+    [id]     INT        NOT NULL,
+    [campo1] NCHAR (10) NOT NULL,
+    [campo2] CHAR(10) NULL, 
+    CONSTRAINT [PK_Exemplo1] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
