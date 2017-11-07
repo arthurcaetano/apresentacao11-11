4820c8d7cc4785c027ce6b42c2f0182d0b710f16
Arthur <Arthur@ARTHUR>
2017-11-06 23:09:22 -0200

    Campo2 removido para fins didáticos.

--- Database3/dbo/Tables/Exemplo1.sql
+++ Database3/dbo/Tables/Exemplo1.sql
@@ -1,7 +1,6 @@
 ﻿CREATE TABLE [dbo].[Exemplo1] (
     [id]     INT        NOT NULL,
     [campo1] NCHAR (10) NOT NULL,
-    [campo2] CHAR(10) NULL, 
     CONSTRAINT [PK_Exemplo1] PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
