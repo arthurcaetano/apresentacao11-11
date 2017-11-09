2111f9e96486cf48de4acbd3f4a57fee21eedd71
Arthur <Arthur@ARTHUR>
2017-11-09 20:57:07 -0200

    Criação do banco de dados de apresentação.

---
+++ Apresentacao/dbo/Tables/Exemplo1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Exemplo1] (
+    [id]       INT          NOT NULL,
+    [campo123] NCHAR (10)   NOT NULL,
+    [campo222] VARCHAR (50) NULL,
+    CONSTRAINT [PK_Exemplo1] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
--- Database3/dbo/Tables/Exemplo1.sql
+++ Database3/dbo/Tables/Exemplo1.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Exemplo1] (
     [id]     INT        NOT NULL,
-    [campo1] NCHAR (10) NOT NULL,
+    [campo12] NCHAR (10) NOT NULL,
     CONSTRAINT [PK_Exemplo1] PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
