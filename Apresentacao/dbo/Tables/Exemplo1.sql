21153b1bdb0e51d764efc079b0e6cd157d9f82ed
Arthur <Arthur@ARTHUR>
2017-11-09 20:59:20 -0200

    Alteração dos nomes dos campos da tabela Exemplo1.

--- Apresentacao/dbo/Tables/Exemplo1.sql
+++ Apresentacao/dbo/Tables/Exemplo1.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Exemplo1] (
     [id]       INT          NOT NULL,
-    [campo123] NCHAR (10)   NOT NULL,
-    [campo222] VARCHAR (50) NULL,
+    [campo1] NCHAR (10)   NOT NULL,
+    [campo2] VARCHAR (50) NULL,
     CONSTRAINT [PK_Exemplo1] PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
