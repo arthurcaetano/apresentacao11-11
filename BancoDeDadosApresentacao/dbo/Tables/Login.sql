db6382fd898faf3f3cd4ed584b4a6f27cf589ee9
Arthur <Arthur@ARTHUR>
2017-11-11 10:07:51 -0200

    Criação do projeto

---
+++ BancoDeDadosApresentacao/dbo/Tables/Login.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Login] (
+    [id]     INT          NOT NULL,
+    [login]  NCHAR (10)   NOT NULL,
+    [senha]  VARCHAR (50) NULL,
+    [token2] NCHAR (10)   NULL,
+    CONSTRAINT [PK_Exemplo1] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
---
+++ BancoDeDadosApresentacao/dbo/Tables/TabelaTeste.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TabelaTeste] (
+    [Id]      INT          NOT NULL,
+    [teste11] VARCHAR (50) NULL,
+    [teste22] VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
