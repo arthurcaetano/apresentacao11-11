920d89dfb00c636c16ed8b71a79f1124025a34f7
Arthur <Arthur@ARTHUR>
2017-11-11 08:51:37 -0200

    Comite do projeto

--- Apresentacao/dbo/Tables/Exemplo1.sql
+++ Apresentacao/dbo/Tables/Exemplo1.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Exemplo1] (
     [id]       INT          NOT NULL,
-    [campo1] NCHAR (10)   NOT NULL,
+    [campo122] NCHAR (10)   NOT NULL,
     [campo2] VARCHAR (50) NULL,
     CONSTRAINT [PK_Exemplo1] PRIMARY KEY CLUSTERED ([id] ASC)
 );
